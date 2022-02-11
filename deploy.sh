44bbcfe610afeff2c3696552d2eb45723278f767
Marcy <mm.leitea@outlook.com>
2022-02-10 22:00:20 -0300

    :construction: deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f https://github.com/MarcyMagi/pokemon-trade-front.git master:gh-pages
+
+cd -
+
+rm -r dist
